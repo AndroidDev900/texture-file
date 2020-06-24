3d83c1f21e38741c3f036e46637c884ba2185f60
rhenzalfred <67321356+rhenzalfred@users.noreply.github.com>
2020-06-24 19:07:36 +0800

    Create TextureException.java

---
+++ Android-Texture-File/src/com/textd/TextureException.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2008 dev 900.
+ * 
+ * This file is texture archive
+ *
+ * Java 1.5 Android Texture Archive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java 1.5 Android Texture Archive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java 1.5 Archive Texture.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Created on 06/22/2020
+ */
+package com.textd;
+
+public class TextureException extends Exception {
+    static final long serialVersionUID = -28272625157L;
+   public TextureException(String mess) {
+   super(mess);
+  }
+}
