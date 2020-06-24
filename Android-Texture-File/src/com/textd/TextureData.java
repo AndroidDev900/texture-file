c822256cc6a4f9b1c622b4ee59e4bf7f9cf13776
rhenzalfred <67321356+rhenzalfred@users.noreply.github.com>
2020-06-24 19:06:15 +0800

    Create TextureData.java

---
+++ Android-Texture-File/src/com/textd/TextureData.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2008 dev 900.
+ * 
+ * This file is texture archive
+ *
+ * Java 1.5 Android Texture Archive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java 1.5 Android Texture Archive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java 1.5 Archive Texture.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Created on 06/22/2020
+ */
+package com.textd;
+
+public class TextureData {
+    public static final int DATA1 = 111;
+    public static final int DATA2 = 222;
+    public static final int DATA3 = 333;
+    public static final int DATA4 = 444;
+    public static final int DATA5 = 555;
+   public static final String TYPE1 = "ALPHA_8";
+   public static final String TYPE2 = "RGB565";
+   public static final String TYPE3 = "ARGB_4444";
+   public static final String TYPE4 = "ARGB_8888";
+   public static final int ALPHA_8 = 0x01;
+   public static final int RGB_565 = 0x02;
+   public static final int ARGB_4444 = 0x03;
+   public static final int ARGB_8888 = 0x04;
+ public static final String TEXDATA = "TEXDATA";
+ public static final String TYPE = "TYPE:";
+ public static final String TEX = "tex";
+}
